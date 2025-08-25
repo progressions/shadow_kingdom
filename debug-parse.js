41c410cd169efb986df3dcd09c680ec97c38fab8
Isaac Priestley <progressions@gmail.com>
2025-08-25 08:43:15 -0400

    start over

--- debug-parse.js
+++
@@ -1,8 +0,0 @@
-const { parseTalkCommand } = require('./src/utils/articleParser');
-
-console.log('Testing parseTalkCommand:');
-console.log('Input: ["to", "that", "guardian"]');
-console.log('Output:', parseTalkCommand(["to", "that", "guardian"]));
-console.log();
-console.log('Input: ["guardian"]');
-console.log('Output:', parseTalkCommand(["guardian"]));
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-export declare function main(): Promise<void>;
-export declare class GameCLI {
-    constructor();
-}
-//# sourceMappingURL=index.d.ts.map
